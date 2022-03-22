cf3fc2c4dfc3b75105e3a45c29ee8b3e69eeca34
Benjamin Wilhelm <benjamin.wilhelm@knime.com>
2022-03-22 17:32:37 +0100

    AP-14754: Add BundledCondaChannel extension point
    AP-14754 (Create extension points, plug-ins and features for bundling)

---
+++ org.knime.conda.envbundling/src/org/knime/conda/envbundling/CondaEnvironmentBundlingUtils.java
@@ -0,0 +1,97 @@
+/*
+ * ------------------------------------------------------------------------
+ *
+ *  Copyright by KNIME AG, Zurich, Switzerland
+ *  Website: http://www.knime.com; Email: contact@knime.com
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME AG herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ---------------------------------------------------------------------
+ *
+ * History
+ *   Mar 21, 2022 (benjamin): created
+ */
+package org.knime.conda.envbundling;
+
+import java.io.IOException;
+
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Path;
+import org.knime.core.util.FileUtil;
+import org.osgi.framework.Bundle;
+
+/**
+ * Static utilities for Conda environment bundling.
+ *
+ * @author Benjamin Wilhelm, KNIME GmbH, Konstanz, Germany
+ */
+final class CondaEnvironmentBundlingUtils {
+
+    private CondaEnvironmentBundlingUtils() {
+    }
+
+    /**
+     * Get the absolute, normalized, local path to the file in the specified bundle.
+     *
+     * @param bundle the bundle
+     * @param path path relative to the bundle root
+     * @return the absolute, normalized, local path to the file
+     * @throws IOException if resolving the path failed
+     */
+    static String getAbsolutePath(final Bundle bundle, final String path) throws IOException {
+        return getAbsolutePath(bundle, new Path(path));
+    }
+
+    /**
+     * Get the absolute, normalized, local path to the file in the specified bundle.
+     *
+     * @param bundle the bundle
+     * @param path path relative to the bundle root
+     * @return the absolute, normalized, local path to the file
+     * @throws IOException if resolving the path failed
+     */
+    static String getAbsolutePath(final Bundle bundle, final Path path) throws IOException {
+        final var pkgsUrl = FileLocator.find(bundle, path, null);
+        if (pkgsUrl == null) {
+            throw new IOException(
+                "Could not find the file '" + path + "' in bundle '" + bundle.getSymbolicName() + "'.");
+        }
+        final var pkgsFile = FileUtil.getFileFromURL(FileLocator.toFileURL(pkgsUrl));
+        return pkgsFile.toPath().normalize().toAbsolutePath().toString();
+    }
+}

969350689d8647a08bd10394574bf85846fc131d
Marc Lehner <marc.lehner@knime.com>
2026-01-07 10:37:37 +0100

    AP-25245: make pixi port compatible with python script nodes
    AP-25245 ()

--- org.knime.pixi.nodes/src/org/knime/pixi/nodes/PixiEnvironmentPortObject.java
+++ org.knime.pixi.nodes/src/org/knime/pixi/nodes/PixiEnvironmentPortObject.java
@@ -1,9 +1,11 @@
 package org.knime.pixi.nodes;
 
+
 import java.nio.file.Path;
 
 import javax.swing.JComponent;
 
+import org.knime.conda.CondaEnvironmentDirectory;
 import org.knime.core.node.CanceledExecutionException;
 import org.knime.core.node.ExecutionMonitor;
 import org.knime.core.node.InvalidSettingsException;
@@ -69,7 +71,7 @@ public final class PixiEnvironmentPortObject extends AbstractSimplePortObject {
     private static Path resolveExecutable(final Path baseDir, final String executableName) {
         Path envDir = baseDir.resolve(".pixi").resolve("envs").resolve("default");
         boolean isWin = System.getProperty("os.name").toLowerCase().contains("win");
-        return isWin ? envDir.resolve(executableName + ".exe") : envDir.resolve("bin").resolve("executableName");
+        return isWin ? envDir.resolve(executableName + ".exe") : envDir.resolve("bin").resolve(executableName);
     }
 
     /**
@@ -84,6 +86,26 @@ public final class PixiEnvironmentPortObject extends AbstractSimplePortObject {
      */
     public Path getRExecutablePath() {
         return resolveExecutable(m_pixiEnvironmentPath, "r");
+    }
+
+    /**
+     * Returns a CondaEnvironmentDirectory for this Pixi environment. This allows reusing conda-related
+     * infrastructure for pixi environments.
+     *
+     * @return a CondaEnvironmentDirectory representing this Pixi environment
+     */
+    public CondaEnvironmentDirectory getAsCondaEnvironmentDirectory() {
+        final Path pythonExec = getPythonExecutablePath();
+        final Path envDir = pythonExec.getParent();
+        final String envDirPath;
+        if (System.getProperty("os.name").toLowerCase().contains("win")) {
+            // On Windows, python.exe is directly in the environment directory
+            envDirPath = envDir.toString();
+        } else {
+            // On Linux/macOS, python is in bin/, so we need the parent of bin/
+            envDirPath = envDir.getParent().toString();
+        }
+        return new CondaEnvironmentDirectory(envDirPath);
     }
 
     @Override
